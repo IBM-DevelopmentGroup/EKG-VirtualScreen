d28efe4f11256fb5e1a32a95905106bb25360828
AmadiL <amadeusz.amadi@gmail.com>
2016-06-03 16:08:19 +0200

    PIL removed, using QImage as paint device.

--- VirtualScreen.py
+++ VirtualScreen.py
@@ -1,40 +1,48 @@
 #!/usr/bin/python
 """Python 3.4.4
 PyQt 4.8.7
-Pillow 3.2.0
 """
 
 import sys
-from PIL import Image
 from PyQt4.QtGui import *
+from PyQt4.QtCore import *
 import time
 
 
+class MainWindow(QDialog):
+
+    def __init__(self):
+        QDialog.__init__(self)
+
+        layout = QVBoxLayout()
+
+        screen = QLabel()
+        screen_img = QImage(800, 480, QImage.Format_RGB32)
+        for i in range(800):
+            for j in range (480):
+                screen_img.setPixel(i, j, qRgb(0,0,0))
+
+        screen_img = QPixmap.fromImage(screen_img)
+        screen.setPixmap(screen_img)
+
+        layout.addWidget(screen)
+
+        self.setLayout(layout)
+
+        self.setWindowTitle("EKG Virtual Screen")
+        self.setFocus()
+
+
 def main():
-    img_file = "my_image.png"
-    img = Image.new('RGB', (800, 480), "black")  # create a new black image
-    pixels = img.load()  # create the pixel map
+    """main function
 
-    for i in range(img.size[0]):  # for every pixel:
-        for j in range(img.size[1]):
-            pixels[i, j] = (i, j, 100)  # set the colour accordingly
-
-    img.save(img_file, "png")
-    image = Image.open(img_file)
+    :return: None
+    """
 
     app = QApplication(sys.argv)
-    win = QWidget()
-    l1 = QLabel()
-    l1.setPixmap(QPixmap(img_file))
-
-    vbox = QVBoxLayout()
-    vbox.addWidget(l1)
-    win.setLayout(vbox)
-    win.setWindowTitle("QPixmap Demo")
+    win = MainWindow()
     win.show()
-
-    win.show()
-    sys.exit(app.exec_())
+    return app.exec()
 
 
 if __name__ == "__main__":
